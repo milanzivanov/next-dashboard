5b14f461b7e2a13733fd36ac7f7971a0410c754b
milan <milanzivanov@users.noreply.github.com>
2025-04-01 17:23:52 +0200

    Creating an invoice

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,6 +1,6 @@
-import { clsx } from 'clsx';
-import Link from 'next/link';
-import { lusitana } from '@/app/ui/fonts';
+import { clsx } from "clsx";
+import Link from "next/link";
+import { lusitana } from "@/app/ui/fonts";
 
 interface Breadcrumb {
   label: string;
@@ -9,19 +9,21 @@ interface Breadcrumb {
 }
 
 export default function Breadcrumbs({
-  breadcrumbs,
+  breadcrumbs
 }: {
   breadcrumbs: Breadcrumb[];
 }) {
+  // console.log("/////////////", breadcrumbs);
+
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
-      <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
+      <ol className={clsx(lusitana.className, "flex text-xl md:text-2xl")}>
         {breadcrumbs.map((breadcrumb, index) => (
           <li
             key={breadcrumb.href}
             aria-current={breadcrumb.active}
             className={clsx(
-              breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
+              breadcrumb.active ? "text-gray-900" : "text-gray-500"
             )}
           >
             <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
