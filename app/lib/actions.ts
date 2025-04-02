1a32ad1c11b77f5d79e5992dc1d28e6dde82ba71
milan <milanzivanov@users.noreply.github.com>
2025-04-02 13:14:14 +0200

    Updating an invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -31,9 +32,27 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-  console.log("///////////", customerId, amount, status);
+  // console.log("///////////", customerId, amount, status);
 
   revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status")
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
