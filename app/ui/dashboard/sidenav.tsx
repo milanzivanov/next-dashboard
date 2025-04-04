dd384cb9243fca73081fbe5ac116775c69636872
milan <milanzivanov@users.noreply.github.com>
2025-04-04 22:40:53 +0200

    Adding login with google and github

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,9 +1,14 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import { auth, signOut } from "@/auth/auth";
 
-export default function SideNav() {
+export default async function SideNav() {
+  const session = await auth();
+
+  // console.log("session", session);
+
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -15,12 +20,28 @@ export default function SideNav() {
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <p>
+          <img
+            className="inline-block h-8 w-8 rounded-full ring-2 ring-white mr-1"
+            src={session?.user?.image ?? ""}
+            alt={session?.user?.name ?? "User"}
+          />
+          Hello
+        </p>
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-        <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button
+            type="submit"
+            className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          >
             <PowerIcon className="w-6" />
-            <div className="hidden md:block">Sign Out</div>
+            Signout
           </button>
         </form>
       </div>
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,67 +1,91 @@
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from "@/app/ui/fonts";
 import {
   AtSymbolIcon,
   KeyIcon,
-  ExclamationCircleIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+  ExclamationCircleIcon
+} from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { Button } from "./button";
+import { signIn } from "@/auth/auth";
 
 export default function LoginForm() {
   return (
-    <form className="space-y-3">
+    <div className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-        </div>
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
+        <h1 className={`${lusitana.className} mb-3 text-2xl`}>Login with</h1>
+
+        <form
+          action={async () => {
+            "use server";
+            await signIn("google");
+          }}
+        >
+          <Button className="mt-4 w-full" type="submit">
+            Google <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </Button>
+        </form>
+
+        <form
+          action={async () => {
+            "use server";
+            await signIn("github");
+          }}
+        >
+          <Button className="mt-4 w-full" type="submit">
+            GitHub <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </Button>
+        </form>
+
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
         </div>
       </div>
-    </form>
+    </div>
   );
 }
+
+// export default async function SignIn() {
+//   const session = await auth();
+
+//   return (
+//     <div>
+//       {session?.user ? (
+//         <div className="flex flex-col items-center justify-center h-screen">
+//           <h1 className="text-2xl">Welcome {session.user.name}</h1>
+//           <form
+//             action={async () => {
+//               "use server";
+//               await signOut();
+//             }}
+//           >
+//             <button
+//               type="submit"
+//               className="p-2 bg-red-700 text-white mx-auto rounded-md hover:bg-red-800"
+//             >
+//               Signout
+//             </button>
+//           </form>
+//         </div>
+//       ) : (
+//         <>
+//           <div className="flex flex-col items-center justify-center h-screen">
+//             <h1 className="text-2xl">Please Sign In</h1>
+//             <form
+//               action={async () => {
+//                 "use server";
+//                 await signIn("google");
+//               }}
+//             >
+//               <button
+//                 type="submit"
+//                 className="p-2 bg-blue-700 text-white mx-auto rounded-md hover:bg-blue-800"
+//               >
+//                 Signin
+//               </button>
+//             </form>
+//           </div>
+//         </>
+//       )}
+//     </div>
+//   );
+// }
