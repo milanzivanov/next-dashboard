569786854dd707f03ecb75a35a2978dacd092218
milan <milanzivanov@users.noreply.github.com>
2025-04-08 13:40:05 +0200

    Code cleaning

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -13,31 +13,25 @@ export default async function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       {session?.user?.image ? (
-        <img
+        <Image
           className="inline-block h-12 w-12 rounded-full ring-2 bg-white ring-white mr-2"
           src={session?.user?.image}
           alt={session?.user?.name ?? "User Image"}
         />
       ) : (
-        <DefaultImage />
+        <Image
+          src={
+            "https://ovkrqbwfxzmoblsaodse.supabase.co/storage/v1/object/public/cultural-hero-avatars//avatar1.jpg"
+          }
+          alt="Default user"
+          width={48}
+          height={48}
+          className="inline-block h-12 w-12 rounded-full ring-2 mr-2 grey bg-white ring-white grayscale"
+          priority={true}
+        />
       )}
 
       <h1 className="text-[44px] capitalize">{firstName ?? "Acme"}</h1>
     </div>
-  );
-}
-
-function DefaultImage() {
-  return (
-    <Image
-      src={
-        "https://ovkrqbwfxzmoblsaodse.supabase.co/storage/v1/object/public/cultural-hero-avatars//avatar1.jpg"
-      }
-      alt="Default user"
-      width={48}
-      height={48}
-      className="inline-block h-12 w-12 rounded-full ring-2 mr-2 grey bg-white ring-white grayscale"
-      priority={true}
-    />
   );
 }
